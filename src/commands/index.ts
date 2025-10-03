c5aab5ec7a63e74b6f569fa04cb5da31e3dc71d9
Sebastian Vallin <29126461+Sebbev@users.noreply.github.com>
2025-10-03 16:44:15 +0200

    feat: Register project commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,8 +1,10 @@
 import type { Command } from "commander";
 import { registerAuthenticationCommands } from "./authentication";
 import { registerEnvironmentCommands } from "./environment";
+import { registerProjectCommands } from "./project";
 
 export function registerCommands(program: Command) {
 	registerAuthenticationCommands(program);
 	registerEnvironmentCommands(program);
+	registerProjectCommands(program);
 }
