3add73e52dd86da3581fd2933ce397081209e931
Sebastian Vallin <29126461+Sebbev@users.noreply.github.com>
2025-10-06 10:49:37 +0200

    feat(project): enhance 'info' command to fetch project details with error handling and environment listing

--- src/commands/project.ts
+++ src/commands/project.ts
@@ -1,3 +1,4 @@
+import { select } from "@inquirer/prompts";
 import { Command } from "commander";
 import * as pc from "picocolors";
 import { initOpenAPIConfig } from "@/config";
@@ -20,19 +21,96 @@ function info(program: Command) {
 	program
 		.command("info")
 		.description("Get information about a project")
-		.option("-p, --project [project]", "Project ID")
-		.action(async () => {
+		.option("-p, --project <PROJECT_ID>", "Project ID")
+		.action(async function () {
 			type Options = {
 				project?: string;
 			};
 
 			initOpenAPIConfig();
 
-			const options = program.opts<Options>();
+			const options = this.opts<Options>();
 
 			let project: Project;
 			if (options.project) {
+				try {
+					project = await ProjectService.projectOne(options.project);
+				} catch (error) {
+					if (error instanceof ApiError) {
+						this.error(pc.red(`Failed to get project info: ${error.message}`));
+					} else if (error instanceof Error) {
+						this.error(pc.red(`Failed to get project info: ${error.message}`));
+					} else {
+						this.error(pc.red("An unknown error occurred while fetching project info."));
+					}
+				}
 			} else {
+				try {
+					const projects: Project[] = await ProjectService.projectAll();
+					projects.sort((a, b) => a.name.localeCompare(b.name));
+
+					project = await select({
+						message: pc.blue("Select Project"),
+						choices: projects.map((project) => ({
+							name: project.name,
+							value: project,
+						})),
+					});
+				} catch (error) {
+					if (error instanceof ApiError) {
+						this.error(pc.red(`Failed to get project info: ${error.message}`));
+					} else if (error instanceof Error) {
+						this.error(pc.red(`Failed to get project info: ${error.message}`));
+					} else {
+						this.error(pc.red("An unknown error occurred while fetching project info."));
+					}
+				}
+			}
+
+			console.log(pc.bold(pc.blue("\nProject Information:")));
+			console.log(pc.bold("  Name:"), pc.cyan(project.name));
+			console.log(pc.bold("  Description:"), pc.cyan(project.description || "No description"));
+			console.log(pc.bold("  Created At:"), pc.cyan(new Date(project.createdAt).toLocaleString()));
+
+			if (!project.environments.length) {
+				console.log(pc.yellow("\nNo environments found in this project."));
+			}
+			for (const env of project.environments) {
+				console.log(pc.bold(pc.green(`\n${env.name} (environment)`)));
+				console.log(pc.bold(`  Applications(${env.applications.length}):`));
+				env.applications.forEach((app) => {
+					console.log(`    - ${pc.cyan(app.name)} (${app.appName})`);
+				});
+
+				console.log(pc.bold(`  Compose Services(${env.compose.length}):`));
+				env.compose.forEach((compose) => {
+					console.log(`    - ${pc.cyan(compose.name)} (${compose.appName})`);
+				});
+
+				console.log(pc.bold(`  MariaDB Instances(${env.mariadb.length}):`));
+				env.mariadb.forEach((mdb) => {
+					console.log(`    - ${pc.cyan(mdb.name)} (${mdb.appName})`);
+				});
+
+				console.log(pc.bold(`  MongoDB Instances(${env.mongo.length}):`));
+				env.mongo.forEach((mdb) => {
+					console.log(`    - ${pc.cyan(mdb.name)} (${mdb.appName})`);
+				});
+
+				console.log(pc.bold(`  MySQL Instances(${env.mysql.length}):`));
+				env.mysql.forEach((mysqldb) => {
+					console.log(`    - ${pc.cyan(mysqldb.name)} (${mysqldb.appName})`);
+				});
+
+				console.log(pc.bold(`  PostgreSQL Instances(${env.postgres.length}):`));
+				env.postgres.forEach((pgdb) => {
+					console.log(`    - ${pc.cyan(pgdb.name)} (${pgdb.appName})`);
+				});
+
+				console.log(pc.bold(`  Redis Instances(${env.redis.length}):`));
+				env.redis.forEach((redis) => {
+					console.log(`    - ${pc.cyan(redis.name)} (${redis.appName})`);
+				});
 			}
 		});
 }
--- src/types.ts
+++ src/types.ts
@@ -16,11 +16,11 @@ export interface Environment {
 	env: string;
 	projectId: string;
 	applications: Application[];
-	// mariadb: MariaDB[];
-	// mongo: MongoDB[];
-	// mysql: MySQL[];
-	// postgresql: PostgreSQL[];
-	// redis: Redis[];
+	mariadb: MariaDB[];
+	mongo: MongoDB[];
+	mysql: MySQL[];
+	postgres: PostgreSQL[];
+	redis: Redis[];
 	compose: Compose[];
 }
 
@@ -40,6 +40,31 @@ export interface Compose {
 	description: string;
 	env: string;
 	// more fields...
+}
+
+export interface MariaDB {
+	name: string;
+	appName: string;
+}
+
+export interface MongoDB {
+	name: string;
+	appName: string;
+}
+
+export interface MySQL {
+	name: string;
+	appName: string;
+}
+
+export interface PostgreSQL {
+	name: string;
+	appName: string;
+}
+
+export interface Redis {
+	name: string;
+	appName: string;
 }
 
 export interface Project {
